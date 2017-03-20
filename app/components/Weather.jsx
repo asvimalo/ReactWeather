5e6a1a0f47a6c55c3c0938dbbff51262d9950c6f
circoloco <asvimalo@gmail.com>
2017-03-20 22:03:29 +0100

    Some more changes

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var WeatherForm = require('./WeatherForm');
 var WeatherMessage = require('./WeatherMessage');
 var openWeatherMap = require('../../api/openWeatherMap');
+var ErrorModal = require('./ErrorModal')
 
 var Weather = React.createClass({
 
@@ -13,7 +14,10 @@ var Weather = React.createClass({
   handleSearch: function (location) {
     var that = this; //This needs to be outside then
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
@@ -21,15 +25,18 @@ var Weather = React.createClass({
         location: location,
         temp: temp
       });
-    }, function (errorMessage) {
-      that.setState({isLoading: false});
-      alert(errorMessage);
-    })
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
+    });
   },
   render: function () {
     var isLoading = this.state.isLoading;
     var location = this.state.location;
     var temp = this.state.temp;
+    var errorMessage = this.state.errorMessage;
 
     function renderMessage () {
       if (isLoading) {
@@ -38,12 +45,21 @@ var Weather = React.createClass({
         return  <WeatherMessage location={location} temp={temp}/>
       }
     }
+    function renderError () {
+      if(typeof errorMessage === 'string') {
+        return (
+          <ErrorModal/>
+        )
+
+      }
+    }
     //var {temp,location} = this.state;
     return (
       <div>
         <h3>Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
